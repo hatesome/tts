eda235e2ae173d82365b76b258bb8dcd763585ab
hatesome <107082340+hatesome@users.noreply.github.com>
2024-10-11 12:46:37 +0300

    Add files via upload

---
+++ maintheme/maintheme/search.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * The template for displaying search results pages
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/#search-result
+ *
+ * @package maintheme
+ */
+
+get_header();
+global $wp_query;
+?>
+<div class="wapper">
+  <div class="contentarea clearfix">
+    <div class="content"><?php echo do_shortcode('[elementor_short id="1772"]'); ?></div>
+  </div>
+</div>
+
+<?php
+get_footer();
+
+
+
+
+
+
+
+
